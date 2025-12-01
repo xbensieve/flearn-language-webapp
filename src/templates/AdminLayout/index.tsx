2414a1e64ae7dbb6aa5cf3046415c552c17950d3
xbensieve <bennguyen.contact@gmail.com>
2025-12-01 19:47:27 +0700

    fix the logout admin

--- src/templates/AdminLayout/index.tsx
+++ src/templates/AdminLayout/index.tsx
@@ -26,7 +26,7 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { logoutService } from "../../services/auth";
 import { getAdminWalletService } from "../../services/payout";
 import { toast } from "react-toastify";
-import { notifySuccess } from "@/utils/toastConfig";
+import { notifyError, notifySuccess } from "@/utils/toastConfig";
 
 const { Header, Sider, Content } = Layout;
 const { Text } = Typography;
@@ -50,11 +50,11 @@ const DashboardLayout: React.FC = () => {
     onSuccess: () => {
       localStorage.removeItem("FLEARN_ACCESS_TOKEN");
       localStorage.removeItem("FLEARN_REFRESH_TOKEN");
-      toast.success("Đăng xuất thành công!");
-      window.location.href = "/login";
+      notifySuccess("Logout successful");
+      navigate("/login");
     },
     onError: (error: any) => {
-      toast.error(error.message);
+      notifyError(error.message);
     },
   });
 
